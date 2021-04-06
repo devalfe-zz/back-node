38cac7d6eeedadad38333c6fff963b3ab9ec61e3
Devalfe <mdt.webmaster@gmail.com>
2021-04-06 12:06:37 -0500

    feat: no vars

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,5 @@
 import Router from 'express-promise-router';
 import Controllers from '../controllers';
-import { check } from 'express-validator';
-import Middleware from '../middleware';
 const router = Router();
 
 router.post('/login', Controllers.Auth.login);
