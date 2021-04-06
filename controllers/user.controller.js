7dd1f5d3bd3ca9a0165b934375f77165424640ad
Devalfe <mdt.webmaster@gmail.com>
2021-04-06 12:15:38 -0500

    feat: del login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -213,33 +213,5 @@ export default {
       });
       next(e);
     }
-  },
-  login: async (req, res, next) => {
-    try {
-      const user = await models.User.findOne({
-        email: req.body.email,
-        state: 1
-      });
-      if (user) {
-        const match = await bcrypt.compare(req.body.password, user.password);
-        if (match) {
-          const tokenReturn = await token.encode(user.uid);
-          res.status(200).json({ user, tokenReturn });
-        } else {
-          res.status(404).send({
-            message: 'Password Incorrecto'
-          });
-        }
-      } else {
-        res.status(404).send({
-          message: 'No existe el User'
-        });
-      }
-    } catch (e) {
-      res.status(500).send({
-        message: 'Ocurrió un error'
-      });
-      next(e);
-    }
   }
 };
